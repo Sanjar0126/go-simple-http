35d841338e4100ff59527b9a8dc90ab9151a6d89
sanjar <sanjar@>
2025-05-23 15:10:41 +0500

    moved main.go to example showcase

---
+++ example/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"go-simple-http/httpx"
+)
+
+func main() {
+	server := httpx.NewHTTPServer("0.0.0.0", "8080")
+
+	fmt.Println("Starting simple HTTP server...")
+
+	server.Handler = func(req *httpx.HTTPRequest) *httpx.HTTPResponse {
+		fmt.Printf("Custom handler: %s %s\n", req.Method, req.Path)
+		body := fmt.Sprintf("Hello, you requested %s", req.Path)
+		return &httpx.HTTPResponse{
+			Version:    "HTTP/1.1",
+			StatusCode: 200,
+			StatusText: "OK",
+			Headers: map[string]string{
+				"Content-Type":   "text/plain",
+				"Content-Length": fmt.Sprintf("%d", len(body)),
+			},
+			Body: body,
+		}
+	}
+
+	if err := server.Start(); err != nil {
+		fmt.Printf("Server error: %v\n", err)
+	}
+}
