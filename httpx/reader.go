3ad56a927fbde5fd70bfa25fbe332c10347d12f1
sanjar <sanjar@>
2025-05-25 23:34:51 +0500

    properly implement chunkedReader's Read method

--- httpx/reader.go
+++ httpx/reader.go
@@ -2,7 +2,10 @@ package httpx
 
 import (
 	"bufio"
+	"fmt"
 	"io"
+	"strconv"
+	"strings"
 )
 
 type emptyReader struct{}
@@ -12,7 +15,9 @@ func (e *emptyReader) Read(p []byte) (n int, err error) {
 }
 
 type chunkedReader struct {
-	reader *bufio.Reader
+	reader    *bufio.Reader
+	chunkLeft int
+	finished  bool
 }
 
 func newChunkedReader(reader *bufio.Reader) *chunkedReader {
@@ -20,6 +25,54 @@ func newChunkedReader(reader *bufio.Reader) *chunkedReader {
 }
 
 func (c *chunkedReader) Read(p []byte) (n int, err error) {
-	// TODO: Implement chunked reading logic
-	return c.reader.Read(p)
+	if c.finished {
+		return 0, io.EOF
+	}
+
+	if c.chunkLeft == 0 {
+		sizeLine, err := c.reader.ReadString('\n')
+		if err != nil {
+			return 0, err
+		}
+
+		sizeStr := strings.TrimSpace(sizeLine)
+		if idx := strings.Index(sizeStr, ";"); idx != -1 {
+			sizeStr = sizeStr[:idx]
+		}
+
+		chunkSize, err := strconv.ParseInt(sizeStr, 16, 64)
+		if err != nil {
+			return 0, fmt.Errorf("invalid chunk size: %s", sizeStr)
+		}
+
+		c.chunkLeft = int(chunkSize)
+
+		if c.chunkLeft == 0 { //final chunk
+			for {
+				line, err := c.reader.ReadString('\n')
+				if err != nil {
+					return 0, err
+				}
+				if strings.TrimSpace(line) == "" {
+					break
+				}
+			}
+			c.finished = true
+			return 0, io.EOF
+		}
+	}
+
+	toRead := len(p)
+	if toRead > c.chunkLeft {
+		toRead = c.chunkLeft
+	}
+
+	n, err = c.reader.Read(p[:toRead])
+	c.chunkLeft -= n
+
+	if c.chunkLeft == 0 && err == nil {
+		c.reader.ReadString('\n')
+	}
+
+	return n, err
 }
